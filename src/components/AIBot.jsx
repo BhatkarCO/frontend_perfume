60f033595b8bc747c5dc190f6ad1d87a9b10720b
rohanN8740 <rohannandavdekar8983@gmail.com>
2026-07-21 23:07:48 +0530

    Connection

--- src/components/AIBot.jsx
+++ src/components/AIBot.jsx
@@ -39,7 +39,7 @@ export const AIBot = () => {
   setIsTyping(true);
 
   try {
-    const response = await fetch("http://localhost:5000/api/chat", {
+    const response = await fetch("https://backend-perfume-10k4.onrender.com/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
